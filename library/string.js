1f73e79d95019310a67fd94b797e7bac5cfb9ed0
xaota <xaota@users.noreply.github.com>
2018-12-04 11:39:39 +0500

    Форматирование строк

--- library/string.js
+++ library/string.js
@@ -76,4 +76,79 @@
         matches
       };
     }
+
+  /** Отформатированная строка согласно заданному шаблону @static
+    * Поддерживаются следующие спецификаторы: %s, %c, %f, %p %e, %o, %x, %X, %u, %d, %b.
+    * @param {string} string шаблон
+    * @param {array} args даныне для подстановки
+    * @returns {string} Отформатированная строка
+    * @example `'%s %s'.format('This is a string', 11) // => 'This is a string 11'`
+    * @example `'Array: %s'.format(['12.3', 13.6]) // => 'Array: ["12.3",13.6]'`
+    * @example `'Object: %s'.format({test: 'test', id: 12}) // => 'Object: {"test":"test","id":12}'`
+    * @example `'%c'.format('Test') // => 'T'`
+    * @example `'%b %b %b %b'.format('', 0, 1, 'test') // => 'false false true true'`
+    * @example `'%5d'.format(12) // => '   12'`
+    * @example `'%3u'.format(5) // => '  5'`
+    * @example `'%05d'.format(12) // => '00012'`
+    * @example `'%-5d'.format(12) // => '12   '`
+    * @example `'%5.2d'.format(123) // => '  120'`
+    * @example `'%5.2f'.format(1.1) // => ' 1.10'`
+    * @example `'%10.2e'.format(1.1) // => '   1.10e+0'`
+    * @example `'%5.3p'.format(1.12345) // => ' 1.12'`
+    * @example `'%o'.format(12) // => '14'`
+    * @example `'%5x'.format(45054) // => ' affe'`
+    * @example `'%5X'.format(45054) // => ' AFFE'`
+    * @example `'%20#2x'.format('45054') // => '    1010111111111110'`
+    * @example `'%6#2d'.format('111') // => '     7'`
+    * @example `'%6#16d'.format('affe') // => ' 45054'`
+    */
+    static format(string, ...args) {
+      let i = 0;
+
+      return string.replace(/%(-)?(0?[0-9]+)?([.][0-9]+)?([#][0-9]+)?([scbfpeoxXud])/g, (exp, p0, p1, p2, p3, p4) => {
+        if (exp === '%%') return '%';
+        if (p4  === 'b')  return args[++i] ? 'true' : 'false';
+        if (args[++i]) {
+          const exp = p2 && parseInt(p2.substr(1));
+          const base = p3 && parseInt(p3.substr(1));
+          let val;
+          switch (p4) {
+            case 's':
+              val = args[i];
+              break;
+            case 'c':
+              val = args[i][0];
+              break;
+            case 'f':
+              val = parseFloat(args[i]).toFixed(exp);
+              break;
+            case 'p':
+              val = parseFloat(args[i]).toPrecision(exp);
+              break;
+            case 'e':
+              val = parseFloat(args[i]).toExponential(exp);
+              break;
+            case 'o':
+              val = parseInt(args[i]).toString(base || 8);
+              break;
+            case 'x':
+            case 'X':
+              val = parseInt(args[i]).toString(base || 16);
+              if (p4 == 'X') val = val.toUpperCase();
+              break;
+            case 'u':
+            case 'd':
+              val = parseFloat(parseInt(args[i], base || 10).toPrecision(exp)).toFixed(0);
+              break;
+          }
+
+          val = typeof val == 'object' ? JSON.stringify(val) : val.toString(base);
+          const sz = parseInt(p1);
+          const ch = p1 && p1[0] == '0' ? '0' : ' ';
+
+          while (val.length < sz) val = p0 ? val + ch : ch + val;
+          return val
+        }
+      });
+    }
   }
