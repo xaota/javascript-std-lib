68fc1225e5959b82cd4da7744d932471f39d4fb7
xaota <1172619+xaota@users.noreply.github.com>
2020-06-04 21:23:13 +0500

    Deferred

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-export {default as Num}    from './library/number.js';
-export {default as Arr}    from './library/array.js';
-export {default as Str}    from './library/string.js';
-export {default as Obj}    from './library/object.js';
-export {default as Fn}     from './library/functions.js';
-export {default as Moment} from './library/moment.js';
+export {default as Num}      from './library/number.js';
+export {default as Arr}      from './library/array.js';
+export {default as Str}      from './library/string.js';
+export {default as Obj}      from './library/object.js';
+export {default as Fn}       from './library/functions.js';
+export {default as Moment}   from './library/moment.js';
+export {default as Deferred} from './library/deferred.js'
 
 export {default as uuid}   from './library/uuid.js';
---
+++ library/deferred.js
@@ -0,0 +1,35 @@
+/** {Defer} Promise-like объект, способный резолвиться извне @class
+  * @example
+  * const promise = new Deferred();
+  * ...
+  * promise.resolve(), promise.reject();
+  */
+  export default class Deferred {
+  /** Создание отложенного обещания {Deferred} @constructor
+    */
+    constructor() {
+      this.promise = new Promise((resolve, reject) => {
+        this.resolve = resolve;
+        this.reject  = reject;
+      });
+      this.then  = this.promise.then .bind(this.promise);
+      this.catch = this.promise.catch.bind(this.promise);
+      this[Symbol.toStringTag] = 'Promise';
+    }
+
+  /** Проверка на Deferred / is @static
+    * @param {any} value проверяемый объект
+    * @return {boolean} true, если переданный объект {Deferred}
+    */
+    static is(value) {
+      return value instanceof Deferred;
+    }
+
+  /** Проверка на промис / isPromise @static
+    * @param {any} value проверяемый объект
+    * @return {boolean} true, если переданный объект {Promise}
+    */
+    static isPromise(value) {
+      return !!value && (typeof value === 'object' || typeof value === 'function') && typeof value.then === 'function';
+    }
+  }
