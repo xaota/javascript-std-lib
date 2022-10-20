e878e246e95e162aa07add2856b5da5b71ee9f0c
xaota <1172619+xaota@users.noreply.github.com>
2022-10-20 23:42:11 +0300

    debounce func

--- library/functions.js
+++ library/functions.js
@@ -1,6 +1,45 @@
 /** @section @exports */
 /** */
   export default class Fn {
+  /** */
+    static debounce(f, ms) {
+      let isCooldown = false;
+
+      return function() {
+        if (isCooldown) return;
+        f.apply(this, arguments);
+        isCooldown = true;
+        setTimeout(() => isCooldown = false, ms);
+      };
+    }
+
+  /** */
+    static throttle(func, ms) {
+      let isThrottled = false,
+        savedArgs,
+        savedThis;
+
+    /** */
+      function wrapper() {
+        if (isThrottled) { // (2)
+          savedArgs = arguments;
+          savedThis = this;
+          return;
+        }
+
+        func.apply(this, arguments); // (1)
+        isThrottled = true;
+        setTimeout(function() {
+          isThrottled = false; // (3)
+          if (savedArgs) {
+            wrapper.apply(savedThis, savedArgs);
+            savedArgs = savedThis = null;
+          }
+        }, ms);
+      }
+      return wrapper;
+    }
+
   /** Оператор функционального матчинга @static
     * @param {any} args матчевые переменные
     * @return {function} @closure
